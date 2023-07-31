4788e737313ba4a86fc9e4bf77826f28b09d514e
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 22:41:39 +0530

    Create main.java

---
+++ practical 01/Quession 02/main.java
@@ -0,0 +1,9 @@
+public class MyDetails {
+    public static void main(String[] args) {
+        System.out.println("Your Name"); // Replace "Your Name" with your actual name
+        System.out.println("Your Degree Programme"); // Replace "Your Degree Programme" with your actual degree program
+    }
+}
+
+//Your Name
+//Your Degree Programme
