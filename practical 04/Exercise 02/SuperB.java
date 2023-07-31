31e33278ff3e5c86d44864d9b2b8aa6c7c3374e2
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 23:12:11 +0530

    Create SuperB.java

---
+++ practical 04/Exercise 02/SuperB.java
@@ -0,0 +1,18 @@
+class SuperB {
+    int x;
+    void setIt(int n) {
+        x = n;
+    }
+    void increase() {
+        x = x + 1;
+    }
+    void triple() {
+        x = x * 3;
+    }
+    int returnIt() {
+        return x;
+    }
+}
+
+
+//The `SuperB` class has an integer variable `x`, and it provides methods to set its value, increase it by 1, triple its value, and return its current value.
