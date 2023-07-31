17b48cbd2a5821d3853d763e57ff47ab0f5f1696
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 22:57:48 +0530

    Create EmployeeModified.java

---
+++ practical 03/3.1/EmployeeModified.java
@@ -0,0 +1,25 @@
+public class Employee {
+    private String name;
+    private int age;
+    private double salary;
+
+    // Constructor
+    public Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
