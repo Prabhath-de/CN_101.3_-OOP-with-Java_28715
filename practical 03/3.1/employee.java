d3bd76daaea8706778f83cddf76ee5bc3cf996d7
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 22:57:04 +0530

    Create employee.java

---
+++ practical 03/3.1/employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+    private String name;
+    private int age;
+    private double salary;
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
