0d75f1bfcdd3d1ab1e57deb1f23fa440c0114b75
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 22:54:14 +0530

    Create part2.java

---
+++ practical 02/part 02/part2.java
@@ -0,0 +1,17 @@
+//1. 
+   //Answer: b) super
+
+//3. 
+   //Answer: b) private
+
+//4. 
+   //Answer: b) Packages
+
+//5. 
+   //Answer: c) import pkg.*
+
+//6. 
+   //Answer: c) charAt()
+
+//7
+   //Answer: d) length()
