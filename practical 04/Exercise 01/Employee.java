1b77c08c38ccf119dbd35ef993cd26d9bdbbb91a
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 23:05:22 +0530

    Create Employee.java

---
+++ practical 04/Exercise 01/Employee.java
@@ -0,0 +1,31 @@
+public class Employee {
+    private int empID;
+    private String empName;
+    private String empDesignation;
+
+    // Getters
+    public int getEmpID() {
+        return empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public String getEmpDesignation() {
+        return empDesignation;
+    }
+
+    // Setters
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public void setEmpDesignation(String empDesignation) {
+        this.empDesignation = empDesignation;
+    }
+}
