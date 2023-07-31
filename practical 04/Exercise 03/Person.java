07e9d9997c5987acf64bfa55ba97591e721d08b4
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 23:15:06 +0530

    Create Person.java

---
+++ practical 04/Exercise 03/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    private String name;
+    private int id;
+
+    // Constructor
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
