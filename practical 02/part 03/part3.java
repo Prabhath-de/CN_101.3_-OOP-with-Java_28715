13669bbe40e9d4e7f4af31f424e5e110d81d6d8f
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 22:55:36 +0530

    Create part3.java

---
+++ practical 02/part 03/part3.java
@@ -0,0 +1,9 @@
+//1. Real-world objects contain **state** and **behavior**.
+//2. A software object's state is stored in **fields** (or instance variables).
+//3. A software object's behavior is exposed through **methods**.
+//4. Hiding internal data from the outside world, and accessing it only through publicly exposed methods is known as data **encapsulation**.
+//5. A blueprint for a software object is called a **class**.
+//6. Common behavior can be defined in a **base class** and inherited into a **subclass** using the **extends** keyword.
+//7. A collection of methods with no implementation is called an **interface**.
+//8. A namespace that organizes classes and interfaces by functionality is called a **package**.
+//9. The term API stands for **Application Programming Interface**.
