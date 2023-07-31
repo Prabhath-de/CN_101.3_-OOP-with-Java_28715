ef473a9de2f7e621e045e1a2da2db881d3ae17d8
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 22:50:27 +0530

    Create monster.java

---
+++ practical 02/part 01/monster.java
@@ -0,0 +1,8 @@
+public class Monster extends Item 
+{
+    // Constructor for Monster class
+    public Monster(int location, String description) {
+        // Call the superclass constructor using 'super'
+        super(location, description);
+    }
+}
