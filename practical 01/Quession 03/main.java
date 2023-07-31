d1da366cf2b385f26cce15a86d9a370764134af1
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-07-31 22:42:37 +0530

    Create main.java

---
+++ practical 01/Quession 03/main.java
@@ -0,0 +1,11 @@
+public class Loop 
+{
+    public static void main(String[] args) {
+        int i;
+        for (i=0;i<=4;i++) 
+        {
+            System.out.println("Executing Loop " + i);
+           
+        }
+    }
+}
