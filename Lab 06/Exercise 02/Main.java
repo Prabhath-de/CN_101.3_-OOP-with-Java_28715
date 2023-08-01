90795de3643e03fd75df9845f6e2938a884bbc61
Prabhath De Silva <129098348+Prabhath-de@users.noreply.github.com>
2023-08-01 07:08:14 +0530

    Create Main.java

---
+++ Lab 06/Exercise 02/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    Priest priest = new Priest();
+    priest.speak();
+
+    Lecturer lecturer = new Lecturer();
+    lecturer.speak();
+
+    Politician politicial = new Politician();
+    politicial.speak();
+  }
+}
